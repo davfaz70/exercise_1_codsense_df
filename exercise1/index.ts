baf1cd96ca99d70f7decb7a572f756086825ed7c
Davide Fazio <davide.fazio@icloud.com>
2023-09-16 15:42:58 +0200

    feat: generics

---
+++ exercise1/index.ts
@@ -0,0 +1,18 @@
+/* GENERICS */
+const numberArray = [1, 2, 3, 4, 5];
+const doubledArray = transformArray(numberArray, (num) => num * 2);
+console.log(doubledArray); // Output: [2, 4, 6, 8, 10]
+const stringArray = ['apple', 'banana', 'cherry'];
+const uppercasedArray = transformArray(stringArray, (str) => str.toUpperCase());
+console.log(uppercasedArray); // Output: ['APPLE', 'BANANA', 'CHERRY']
+
+
+function transformArray <T> (inputArray: T[], callbackFunction: (arg0: T) => T) {
+    
+    inputArray.forEach((element, i) => {
+        element = callbackFunction(element);
+        inputArray[i] = element;
+    });
+
+    return inputArray;
+}
